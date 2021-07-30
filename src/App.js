e9b47a9f8c24346269954ae243202b6c94b7c086
thanhtb-2411 <tran.ba.thanh@sun-asterisk.com>
2021-07-30 11:01:05 +0700

    test Fb

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="fb.com"
+          href="https://facebook.com"
           target="_blank"
           rel="noopener noreferrer"
         >
