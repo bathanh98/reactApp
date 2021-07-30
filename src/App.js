12b123263f93bfcd9e751a74988df6ab03c51c0c
thanhtb-2411 <tran.ba.thanh@sun-asterisk.com>
2021-07-30 10:14:22 +0700

    test tranbathanh

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Bùi Thanh Thúy
+          TRẦN BÁ THANH
         </p>
         <a
           className="App-link"
