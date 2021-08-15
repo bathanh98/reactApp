3ac6ea2c597a39c5b65edf9e74eb6a6734b52eed
bathanh98 <47908667+bathanh98@users.noreply.github.com>
2021-08-15 16:01:35 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bui Thị Thanh Thúy
+          TRẦN BÁ THANH
         </p>
         <a
           className="App-link"
